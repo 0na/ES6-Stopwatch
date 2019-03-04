bfbd3a3ab66eb3feb0fa19ef11771791a180aac5
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-03-04 16:28:49 +0100

    changes

--- index.js
+++ index.js
@@ -1,97 +1,101 @@
 //const { minutes, seconds, miliseconds } = times;
 
-
 class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
-    } //ok
+  constructor(display) {
+    this.running = false;
+    this.display = display;
+    this.reset();
+    this.print(this.times);
+  } //ok
 
-    reset() {
-        this.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        };
+  reset() {
+    this.times = {
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    };
+  }
+  print() {
+    //ok
+    this.display.innerText = this.format(this.times);
+  }
+
+  // format(times) {
+  //     this.times(pad0) = {
+  //         minutes,
+  //         seconds,
+  //         miliseconds
+  //     };
+  // }
+  format(times) {
+    //przygotowuje tekst do wyswietlenia
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(
+      Math.floor(times.miliseconds)
+    )}`;
+  } //wynik to 02:04:23 (2 min, 4 s, 10 ms)
+
+  start() {
+    //ok
+    if (!this.running) {
+      this.running = true;
+      this.watch = setInterval(() => this.step(), 10);
     }
-    print() { //ok
-        this.display.innerText = this.format(this.times);
+  }
+
+  step() {
+    //ok
+    if (!this.running) return;
+    this.calculate();
+    this.print();
+  }
+
+  // calculate() {
+  //   this.setState({
+  //     times: {
+  //       miliseconds = +1,
+  //       if (miliseconds >= 100) {
+  //         seconds += 1,
+  //         miliseconds = 0,
+  //       }
+  //       if (seconds >= 60) {
+  //         minutes += 1,
+  //         seconds = 0,
+  //       });
+  //       }}}
+
+  calculate() {
+    //ok
+    this.times.miliseconds += 1;
+    if (this.times.miliseconds >= 100) {
+      this.times.seconds += 1;
+      this.times.miliseconds = 0;
     }
-
-    // format(times) {
-    //     this.times(pad0) = {
-    //         minutes,
-    //         seconds,
-    //         miliseconds
-    //     };
-    // }
-    format(times) { //przygotowuje tekst do wyswietlenia
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    } //wynik to 02:04:23 (2 min, 4 s, 10 ms)
-
-    start() { //ok
-        if (!this.running) {
-            this.running = true;
-            this.watch = setInterval(() => this.step(), 10);
-        }
+    if (this.times.seconds >= 60) {
+      this.times.minutes += 1;
+      this.times.seconds = 0;
     }
+  }
 
-    step() { //ok
-        if (!this.running) return;
-        this.calculate();
-        this.print();
-    }
-
-    // calculate() {
-    //     this.times = {
-    //         miliseconds = +1,
-    //         if (this.miliseconds >= 100) {
-    //             seconds = +1,
-    //                 miliseconds = 0;
-    //         }
-    //         if (this.times.seconds >= 60) {
-    //             minutes = +1,
-    //                 seconds = 0,
-    //         }
-    //     }
-    // }
-
-
-
-    calculate() { //ok
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.miliseconds = 0;
-        }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
-        }
-    }
-
-    stop() { //ok
-        this.running = false;
-        clearInterval(this.watch);
-    }
+  stop() {
+    //ok
+    this.running = false;
+    clearInterval(this.watch);
+  }
 }
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch')); //ok
+const stopwatch = new Stopwatch(document.querySelector(".stopwatch")); //ok
 
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start()); //ok
+let startButton = document.getElementById("start");
+startButton.addEventListener("click", () => stopwatch.start()); //ok
 
-let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop()); //ok
-
-
+let stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", () => stopwatch.stop()); //ok
 
 //Funkcja pad0 przyjmuje na wejście wartość liczbową, przekształca ją na stringa, a następnie sprawdza czy długość tego przekształcenia jest mniejsza od 2 dodając tym samym zero przed tę liczbę.
-function pad0(value) { //dodaje 0 do liczb jednocyfrowych
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
+function pad0(value) {
+  //dodaje 0 do liczb jednocyfrowych
+  let result = value.toString();
+  if (result.length < 2) {
+    result = "0" + result;
+  }
+  return result;
 }
